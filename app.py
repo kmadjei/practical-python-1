98d69bd9a9de7206215a3b4f1fd69ce0e562e75d
Kenneth Adjei <[kmadjei25@gmail.com]>
2021-05-27 15:10:31 -0400

    completed the homepage, and added content for the quotes.html and form_submitted.html

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
+# modules required to run the app
 import os
 import json
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
+from random import random
+import math
+
 
 # Init App
 app = Flask(__name__)
 
-
+# renders the home page
 @app.route('/')
 def home():
-    return 'hello' 
+    msg1 = 'Welcome !'
+    msg2 = 'To the world of Flask and Python.'
+    return render_template('home.html', msg1=msg1, msg2=msg2)
+
+
+# renders the confirmation page for the form submitted
+@app.route('/form_submission', methods=['GET', 'POST'])
+def form_submission():
+    
+    msg1 = f"Hello {request.form.get('name')} !"
+    msg2 = f"We now know you like --> {request.form.get('favorite_activity')}"
+    return render_template('form_submitted.html', msg1=msg1, msg2=msg2)
+
+
+# renders the quotes HTML page
+@app.route('/inspirational_quote')
+def get_inspired():
+    
+    # reads the quotes from the JSON data file
+    with open('data/quotes.json', 'r') as json_data:
+        data = json.load(json_data)
+        #generate random number for dict length
+        random_quote = math.floor(random()*len(data))
+
+    msg1 = "Your Inspiration for Today !"
+        
+    return render_template('quotes.html', msg1=msg1, quote=data[random_quote])
 
 
 # Run server
